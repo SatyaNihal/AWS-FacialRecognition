5af0ca679c55803432be35e026629263c6ca38be
SatyaNihal <nihal.kodukula@icloud.com>
2025-08-29 16:10:49 -0400

    added imgs and init code

---
+++ putimages.py
@@ -0,0 +1,19 @@
+
+import boto3
+
+s3 = boto3.resource('s3')
+
+# Get list of objects for indexing
+images=[('image_01.jpg','Nihal Kodukula'),
+      ('image_02.jpg','Nihal Kodukula'),
+      ('image_03.jpg','Nihal Kodukula'),
+      ('image_04.jpg','Cristiano Ronaldo'),
+      ('image_05.jpg','Cristiano Ronaldo'),
+      ]
+
+# Iterate through list to upload objects to S3   
+for image in images:
+    file = open(image[0],'rb')
+    object = s3.Object('testansarbucket','index/'+ image[0])
+    ret = object.put(Body=file,
+                    Metadata={'FullName':image[1]})
